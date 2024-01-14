f0b49f068b9fa10c8dea443f9fe4496060af601a
rohanpaward <rohanpawar2003rds@gmail.com>
2024-01-14 23:28:20 +0530

    nav-bar-changes

--- Script.js
+++ Script.js
@@ -46,28 +46,18 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 2000); // Change slide every 2 seconds
-
-}const cardWrapper = document.getElementById('card-wrapper');
+}
+  const cardWrapper = document.getElementById('card-wrapper');
 const cardWidth = document.querySelector('.card').offsetWidth;
-const cardsToShow = 3;
 let currentSlide = 0;
 
-function updateCardWrapperPosition() {
-  const translateValue = -currentSlide * cardWidth * cardsToShow;
-  cardWrapper.style.transform = `translateX(${translateValue}px)`;
-}
-
 function slide(direction) {
-  const totalCards = document.querySelectorAll('.card').length;
-
   if (direction === 'left' && currentSlide > 0) {
     currentSlide--;
-  } else if (direction === 'right' && currentSlide < totalCards - cardsToShow) {
+  } else if (direction === 'right' && currentSlide < 9) {
     currentSlide++;
   }
 
-  updateCardWrapperPosition();
+  const translateValue = -currentSlide * cardWidth;
+  cardWrapper.style.transform = `translateX(${translateValue}px)`;
 }
-
-// Initial position setup
-updateCardWrapperPosition();
